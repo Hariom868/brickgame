d6d6c1ff293bd376b8e85c0d7ca35d5aee650565
Amritanshu786 <55310911+Amritanshu786@users.noreply.github.com>
2020-10-01 12:57:57 +0530

    Add files via upload

---
+++ practice3(q3).py
@@ -0,0 +1,22 @@
+# Pattern 1-212-32123...
+
+# Reading number of rows
+row = int(input('Enter how many lines? '))
+
+# Generating pattern
+for i in range(1,row+1):
+
+    # for space
+    for j in range(1, row+1-i):
+        print(' ', end='')
+
+    # for decreasing pattern
+    for j in range(i,0,-1):
+        print(j, end='')
+
+    # for increasing pattern
+    for j in range(2,i+1):
+        print(j, end='')
+
+    # Moving to next line
+    print()
